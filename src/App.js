c0a2b8a9eaa2dc07e7970495f555048987ad329c
ShahulBasha <shhl.basha333@gmail.com>
2020-02-11 21:20:19 +0530

    Added Header Component for all screens

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/Homepage.components";
 import ShopPage from "./pages/shop/ShopPage.component";
-import { Route } from "react-router-dom";
+import Header from "./components/header/header.component";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/shop" component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
